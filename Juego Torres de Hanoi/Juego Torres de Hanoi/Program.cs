a58bf8ec5d27f954b3da003e01b97abdda731028
Fernando-Orozco-2006 <fqop06@gmail.com>
2025-02-19 17:44:13 -0600

    Add files via upload

---
+++ Juego Torres de Hanoi/Juego Torres de Hanoi/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+class TorresDeHanoi
+{
+    private Stack<int>[] torres;
+    private int numDiscos;
+
+    public TorresDeHanoi(int n)
+    {
+        numDiscos = n;
+        torres = new Stack<int>[3];
+        for (int i = 0; i < 3; i++)
+            torres[i] = new Stack<int>();
+
+        for (int i = n; i >= 1; i--)
+            torres[0].Push(i);
+    }
+
+    public void Resolver()
+    {
+        MostrarTorres();
+        MoverDiscos(numDiscos, 0, 2, 1);
+    }
+
+    private void MoverDiscos(int n, int origen, int destino, int auxiliar)
+    {
+        if (n > 0)
+        {
+            MoverDiscos(n - 1, origen, auxiliar, destino);
+            MoverUnDisco(origen, destino);
+            MoverDiscos(n - 1, auxiliar, destino, origen);
+        }
+    }
+
+    private void MoverUnDisco(int origen, int destino)
+    {
+        int disco = torres[origen].Pop();
+        torres[destino].Push(disco);
+        Console.WriteLine($"Favor de mover disco {disco} de Torre {origen + 1} a Torre {destino + 1}");
+        MostrarTorres();
+    }
+
+    private void MostrarTorres()
+    {
+        Console.WriteLine("Estado actual de las torres:");
+        for (int i = 0; i < 3; i++)
+        {
+            Console.Write("Torre " + (i + 1) + ": ");
+            foreach (var disco in torres[i])
+                Console.Write(disco + " ");
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+
+    static void Main()
+    {
+        Console.Write("Favor de ingresar el número de discos: ");
+        int numDiscos = int.Parse(Console.ReadLine());
+        TorresDeHanoi hanoi = new TorresDeHanoi(numDiscos);
+        hanoi.Resolver();
+    }
+}
